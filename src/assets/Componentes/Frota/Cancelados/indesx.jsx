bde824e469b084ccae5893b65688fb6274ddb2ac
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-03-30 16:05:24 -0300

    Update indesx.jsx

--- src/assets/Componentes/Frota/Cancelados/indesx.jsx
+++ src/assets/Componentes/Frota/Cancelados/indesx.jsx
@@ -1,7 +1,32 @@
-const Cancelados = () =>{
-    return(
+import { useEffect, useState } from "react";
+
+const Frota = () => {
+    const [frota, setfrota] = useState([]);
+
+    useEffect(() => {
+        fetch('https://my-json-server.typicode.com/monicahillman/cinetag-api/videos')
+            .then(resposta => resposta.json())
+            .then(dados => {
+                setVideos(dados)
+            })
+    }, [])
+
+    return (
         <>
-        teste
+            <Banner imagem="home" />
+            <Titulo>
+                <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+            </Titulo>
+            <section className={styles.container}>
+                {videos.map((video) => {
+                    return <Card {...video} key={video.id} />
+                })}
+            </section>
+        </>
+    )
+}
+
+export default Inicio;
         </>
     )
 }
